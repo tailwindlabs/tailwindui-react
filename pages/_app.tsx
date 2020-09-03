99fbf2f7da9cc928dd25edbb9e51e8270f08e504
Robin Malfait <malfait.robin@gmail.com>
2020-09-03 14:14:28 +0200

    add a tiny KeyCaster component to display keystrokes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,93 @@ import { useRouter } from 'next/router'
 
 import 'tailwindcss/tailwind.css'
 import { ExamplesType } from '../playground-utils/resolve-all-examples'
+import { useDisposables } from '../src/hooks/use-disposables'
+
+enum KeyDisplayMac {
+  ArrowUp = '↑',
+  ArrowDown = '↓',
+  ArrowLeft = '←',
+  ArrowRight = '→',
+  Home = '↖',
+  End = '↘',
+  Alt = '⌥',
+  CapsLock = '⇪',
+  Meta = '⌘',
+  Shift = '⇧',
+  Control = '⌃',
+  Backspace = '⌫',
+  Delete = '⌦',
+  Enter = '↵',
+  Escape = '⎋',
+  Tab = '↹',
+  PageUp = '⇞',
+  PageDown = '⇟',
+  ' ' = '␣',
+}
+
+enum KeyDisplayWindows {
+  ArrowUp = '↑',
+  ArrowDown = '↓',
+  ArrowLeft = '←',
+  ArrowRight = '→',
+  Meta = 'Win',
+  Control = 'Ctrl',
+  Backspace = '⌫',
+  Delete = 'Del',
+  Escape = 'Esc',
+  PageUp = 'PgUp',
+  PageDown = 'PgDn',
+  ' ' = '␣',
+}
+
+function tap<T>(value: T, cb: (value: T) => void) {
+  cb(value)
+  return value
+}
+
+function useKeyDisplay() {
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return {}
+  const isMac = navigator.userAgent.indexOf('Mac OS X') !== -1
+  return isMac ? KeyDisplayMac : KeyDisplayWindows
+}
+
+function KeyCaster() {
+  const [keys, setKeys] = React.useState<string[]>([])
+  const d = useDisposables()
+  const KeyDisplay = useKeyDisplay()
+
+  React.useEffect(() => {
+    function handler(event: KeyboardEvent) {
+      setKeys(current => [
+        event.shiftKey && event.key !== 'Shift'
+          ? KeyDisplay[`Shift${event.key}`] ?? event.key
+          : KeyDisplay[event.key] ?? event.key,
+        ...current,
+      ])
+      d.setTimeout(() => setKeys(current => tap(current.slice(), clone => clone.pop())), 2000)
+    }
+
+    window.addEventListener('keydown', handler)
+    return () => window.removeEventListener('keydown', handler)
+  }, [d, KeyDisplay])
+
+  if (keys.length <= 0) return null
+
+  return (
+    <div className="fixed z-50 px-4 py-2 overflow-hidden text-2xl tracking-wide text-blue-100 bg-blue-800 rounded-md shadow cursor-default pointer-events-none select-none right-4 bottom-4">
+      {keys
+        .slice()
+        .reverse()
+        .join(' ')}
+    </div>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
@@ -32,7 +119,7 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
 
-      <div className="flex flex-col h-screen font-sans antialiased text-gray-900">
+      <div className="flex flex-col h-screen overflow-hidden font-sans antialiased text-gray-900 bg-white">
         <header className="relative z-10 flex items-center justify-between flex-shrink-0 px-4 py-4 bg-white border-b border-gray-200 sm:px-6 lg:px-8">
           <Link href="/">
             <a>
@@ -58,7 +145,9 @@ function MyApp({ Component, pageProps }) {
           )}
         </header>
 
-        <main className="flex-1">
+        <KeyCaster />
+
+        <main className="flex-1 overflow-auto">
           <Component {...pageProps} />
         </main>
       </div>
